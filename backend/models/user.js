51f2013a0f2d9d0b2e1bb9d33c4fd37ce47b8f31
Marko Strba <mark3strba@gmail.com>
2025-03-12 02:08:13 +0100

    Fix typeError in User model

--- backend/models/user.js
+++ backend/models/user.js
@@ -11,6 +11,6 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-const User = new mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
