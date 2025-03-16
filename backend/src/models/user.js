4e9a3568427895027bc6e571bd199356f0ad3c23
Marko Strba <mark3strba@gmail.com>
2025-03-16 19:08:04 +0100

    Add User model

---
+++ backend/src/models/user.js
@@ -0,0 +1,17 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    isVerified: { type: Boolean, default: false },
+    verifCode: String,
+    expVerifCode: Date,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
